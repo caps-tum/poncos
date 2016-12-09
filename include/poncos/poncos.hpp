b4c2ff6b45d321ac8cf24a9f24d1e7a60038dda7
Simon Pickartz <spickartz@eonerc.rwth-aachen.de>
2016-12-09 13:02:38 +0100

    add VM pool

--- include/poncos/poncos.hpp
+++ include/poncos/poncos.hpp
@@ -15,6 +15,17 @@
 #include <condition_variable>
 #include <string>
 #include <vector>
+#include <queue>
+
+struct vm_pool_elemT {
+	std::string name;
+	std::string mac_addr;
+};
+
+struct vm_poolT {
+	std::queue<vm_pool_elemT> free;
+	std::queue<vm_pool_elemT> alloc;
+};
 
 struct sched_configT {
 	std::vector<unsigned char> cpus;
@@ -23,6 +34,7 @@ struct sched_configT {
 
 constexpr size_t SLOTS = 2;
 extern const sched_configT co_configs[SLOTS];
+extern vm_poolT vm_pool;
 
 void read_file(std::string filename, std::vector<std::string> &command_queue);
 std::string cgroup_name_from_id(size_t id);
